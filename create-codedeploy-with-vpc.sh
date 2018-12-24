754676922c416690274f8a3453db1e5624c8e773
Kyoungsu Lee <yikster@gmail.com>
2018-12-24 14:46:04 +0900

    Add springboot 2.1 demo code for codedeploy

--- create-codedeploy-with-vpc.sh
+++ create-codedeploy-with-vpc.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation create-stack --stack-name $1 --region $2 --template-body file://CodeDeployAutoscalingTomcatInVPC.yaml --parameters file://parameters.json --capabilities CAPABILITY_IAM --disable-rollback
+aws cloudformation create-stack --stack-name $1 --region $2 --profile $3 --template-body file://CodeDeployAutoscalingTomcatInVPC.yaml --parameters file://parameters.json --capabilities CAPABILITY_IAM --disable-rollback
